67ec0aafd77e4dfc6812f2b3b4d06ec984a2be11
Edgard Zaragoza <edgardzaragoza719@gmail.com>
2025-10-30 13:12:16 -0700

    Initial Spring Boot project setup

---
+++ src/test/java/com/example/aiwebapplication/AiWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aiwebapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
